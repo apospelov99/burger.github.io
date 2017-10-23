90bcb941247b30c5b86a1af4ba235e8da1f11e00
apospelov13 <small99@hotbox.ru>
2017-10-23 13:37:49 +0300

    v2

---
+++ js/common.js
@@ -0,0 +1,47 @@
+$(function() {
+  /*
+  $('.testimotional_response').slick({
+    arrows: false,
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    initialSlide: 1,
+    centerMode: true,
+    centerPadding: '20px',
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    asNavFor: '.testimotional_comments'
+  });
+
+  $('.testimotional_comments').slick({
+    arrows: false,
+    centerMode: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true
+  });
+  */
+  //Animation FadeIn
+  
+  $(window).scroll(function() {
+    $(".animation").each(function(){
+      var pos = $(this).offset().top;
+      //var winTop = $(window).scrollTop();
+      var winTop = $(window).scrollTop();
+      console.log('pos:' + pos);
+      console.log('winTop:' + winTop);
+      if (pos > (winTop)/2) {
+        //alert('pos' + pos + 'winTop' + winTop);
+        $(this).addClass("animated fadeInUpBig");
+        //$(".fadeUp").removeClass("fadeUp").addClass("animated fadeInUpBig");
+        //$(".fadeDown").removeClass("fadeDown").addClass("animated fadeInDownBig");
+      }
+    });
+  });
+
+
+
+
+});
